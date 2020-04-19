49f41b2a67d34905292c6137fa629894bf0500eb
Asha-art <62064699+Asha-art@users.noreply.github.com>
2020-04-18 20:33:32 -0400

    files

---
+++ contacts.js
@@ -0,0 +1,70 @@
+body{
+    background-color: #161E67 ;
+    color: white;
+}
+#top-nav {
+    background-color: #FF5C5C!important;
+}
+ .about{
+    margin-top: 100 px;
+    padding-top:150px
+   } 
+div, h6{
+    padding:20px;
+}
+h2, h5{
+    font-family: 'Montserrat', sans-serif;
+     color: #FF5C5C;
+}
+h1{
+    font-family: 'Montserrat', sans-serif;
+     color: #FF5C5C;
+     font-weight: bold;
+}
+#jarpix{
+opacity: 50%
+}
+/*https://media.treehugger.com/assets/images/2011/10/jars-of-jam.jpg*/
+/* div.form
+{
+    text-align: center;
+    display: inline-block;
+    flex: center;
+    margin-left: auto;
+    margin-right: auto;
+    margin-bottom: 50px;
+    text-align: left;
+} */
+div.form{
+      text-align:center;
+        margin: auto;
+        width: 100%;
+        padding: 10px;
+}
+#name, #email{
+    width : 30%;
+}
+#msg{
+ height:100px;
+ width: 40%;
+}
+#submit{
+   margin: 20px;
+    width: 100px;
+    background-color: rgb(204, 193, 193);
+}
+#submit:hover{
+    background-color: #FF5C5C ;
+}
+/*
+Hex Values:
+RED= #FF5C5C
+Blue = # 161e67
+White = #FFFFFF
+H1 Monsterrat Extra-bold 800
+Other headers: Monsterrat Semi-bold 600
+p : Montserrat Regular 400
+https://fonts.google.com/specimen/Montserrat?
+selection.family=Montserrat:wght@100;400
+Monsterrat
+*/
